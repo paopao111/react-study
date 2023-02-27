269f90eaa83400acff254188b993d4c481bf1729
Simin Hou <simin.hou@shopee.com>
2023-02-27 21:42:19 +0800

    fix: delte file

--- jira/src/App.tsx
+++ jira/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
@@ -10,9 +10,6 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-
-
-        
         <a
           className="App-link"
           href="https://reactjs.org"
